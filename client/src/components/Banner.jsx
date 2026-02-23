f74c0b1f46c39d4cd0d494b5e4b4c43da68bcc8c
devmetilda <sequierametilda@gmail.com>
2026-02-23 21:53:48 +0530

    Updated

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col items-center justify-center py-24 px-6 md:px-16 lg:px-24 xl:px-32 bg-gradient-to-r from-primary/10 to-secondary/10 rounded-lg text-center gap-6'>
-      <div>
-        <h2 className='text-2xl md:text-3xl font-bold mb-4'>Do You Own a Luxury Car?</h2>
-        <p className='text-lg mb-2'>Monetize your vehicle effortlessly by listing it on CarRental.</p>
-        <p className='text-base text-gray-600'>We take care of insurance,driver verification and secure payments - so you can earn passive income, stress-free.</p>
-        <button className='px-8 py-3 bg-primary text-white rounded-md mt-6 hover:bg-primary/90 transition-colors duration-300'>List Your Car</button>
+    <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#7a1f1f] via-[#5a1414] to-black max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
+      
+      <div className='text-white'>
+        <h2 className='text-3xl font-medium'>Do You Own a Luxury Car?</h2>
+
+        <p className='mt-2 text-white/90'>
+          Monetize your vehicle effortlessly by listing it on CarRental.
+        </p>
+
+        <p className='max-w-130 text-white/80'>
+          We take care of insurance, driver verification and secure payments - so you can earn passive income, stress-free.
+        </p>
+
+        {/* ⭐ DARKER BUTTON */}
+        <button className='px-6 py-2 bg-[#4a1010] hover:bg-[#350b0b] transition-all text-white rounded-lg text-sm mt-4 cursor-pointer'>
+          List Your Car
+        </button>
       </div>
 
-        <img src={assets.banner_car_image} alt="Banner Image" className='w-full h-full object-cover rounded-lg' />
-      
+      <img src={assets.banner_car_image} alt="Banner Image" className='max-h-45 mt-10' />
       
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <>
         <Hero />
         <FeaturedSection />
+        <Banner />
         
       </>
     </div>
