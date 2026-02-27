78a69a9505d40bee00512507752166e1af8ddcc4
devmetilda <sequierametilda@gmail.com>
2026-02-27 21:10:45 +0530

    Updated

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 const Cars = () => {
+
+  const [input, setInput] = useState('')
+
   return (
     <div>
       
@@ -11,6 +15,13 @@ const Cars = () => {
             title="Available Cars" 
             subtitle="Browse our selection of premium vehicles available for your next adventure"
           />
+
+          <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
+            <img src={assets.search_icon} alt="" className='w-4.5 h-4.5 mr-2' />
+            <input onClick={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Search by make, model, or features' className='w-full h-full outline-none text-gray-500' />
+            <img src={assets.filter_icon} alt="" className='w-4.5 h-4.5 mr-2' />
+
+          </div>
         </div>
       </div>
 
