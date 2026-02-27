a161c0bad617be434ff965a189b709bf2add3a30
devmetilda <sequierametilda@gmail.com>
2026-02-27 21:00:36 +0530

    Updated

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const Cars = () => {
   return (
     <div>
       
+      <div className='flex flex-col items-center py-20 bg-gray-100 max-md:px-4'>
+        <div className='text-center max-w-2xl'>
+          <Title 
+            title="Available Cars" 
+            subtitle="Browse our selection of premium vehicles available for your next adventure"
+          />
+        </div>
+      </div>
+
+      <div className='px-6 md:px-16 lg:px-24 xl:px-32 py-16'>
+      </div>
+
     </div>
   )
 }
