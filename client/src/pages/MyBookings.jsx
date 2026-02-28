8502e615fad2d7b6e88427ec0449c23cfd08c447
devmetilda <sequierametilda@gmail.com>
2026-02-28 20:20:47 +0530

    Updated

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const MyBookings = () => {
+
+  const [bookings, setBookings] = useStae([])
+
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData)
+  }
+
+  useEffect(()=>{
+    fetchMyBookings()
+  },[])
+
   return (
-    <div>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl'>
       
+      <Title title='My Bookings'
+        subtitle='View and mangae your all car bookings'/>
+
     </div>
   )
 }
