167c3442d5c402ce21a9af896f70d40b8bf5cf72
devmetilda <sequierametilda@gmail.com>
2026-03-01 15:24:17 +0530

    Updated

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -19,7 +19,8 @@ const MyBookings = () => {
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl'>
       
       <Title title='My Bookings'
-        subtitle='View and mangae your all car bookings'/>
+        subtitle='View and manage your all car bookings'
+        align="left"/>
 
     </div>
   )
