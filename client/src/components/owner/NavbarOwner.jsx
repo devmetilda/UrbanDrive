a154201d69acd62220614c9817edb262fb02917c
devmetilda <sequierametilda@gmail.com>
2026-03-03 15:16:09 +0530

    Updated

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets, dummyUserData } from '../../assets/assets'
 
 const NavbarOwner = () => {
+
+  const user = dummyUserData;
   return (
-    <div>
+    <div className='flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all'>
+      <Link to='/'>
+        <img src={assets.logo} alt="" className="h-7" />
+      
+      </Link>
       
     </div>
   )
--- client/src/components/owner/Title.jsx
+++ client/src/components/owner/Title.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({ title, subtitle }) => {
   return (
     <div>
-      
+      <h1 className='font-medium text-3xl'>
+        {title}
+      </h1>
+
+      <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-[600px]'>
+        {subtitle}
+      </p>
     </div>
   )
 }
