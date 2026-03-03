9f89b48736e26581eb15e5cdf10033821f5f59c2
devmetilda <sequierametilda@gmail.com>
2026-03-03 15:07:12 +0530

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,13 @@ import CarDetails from './pages/CarDetails'
 import MyBookings from './pages/MyBookings'
 import Cars from './pages/Cars'   // ✅ ADD THIS
 import Footer from './components/Footer'
+import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
+import Layout from './pages/owner/Layout' 
+
+
 
 const App = () => {
   
@@ -24,6 +31,13 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path='/owner' element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="add-car" element={<AddCar />} />
+        <Route path="manage-cars" element={<ManageCars />} />
+        <Route path="manage-bookings" element={<ManageBookings />} />
+
+        </Route>
       </Routes>
 
       {!isOwnerPath && <Footer />}
